eae9ae7a6878a3721c3db31d351f4ff4f94074a4
seetide <seetide@gmail.com>
2019-11-07 08:53:27 -0500

    Add Owner index page and controller. closes #12

---
+++ pet-clinic-web/src/main/java/guru/framework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package guru.framework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index.html", "/index"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
