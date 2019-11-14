b4d54c772631734a6a992a26d34d29aa0fbd8ce4
seetide <seetide@gmail.com>
2019-11-14 08:55:10 -0500

    Implement repos.  close #39

---
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/repositories/PetRepository.java
@@ -0,0 +1,7 @@
+package guru.framework.sfgpetclinic.repositories;
+
+import guru.framework.sfgpetclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepository extends CrudRepository<Pet, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,7 @@
+package guru.framework.sfgpetclinic.repositories;
+
+import guru.framework.sfgpetclinic.model.Specialty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+}
