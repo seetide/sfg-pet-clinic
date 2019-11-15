2331bcda39b9e73e19763fb84387902d6cd1cb48
seetide <seetide@gmail.com>
2019-11-15 14:03:36 -0500

    Add Project lombok and builder function. Close #51

--- pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/Pet.java
@@ -1,9 +1,17 @@
 package guru.framework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 @Entity
 @Table(name = "pets")
@@ -29,40 +37,4 @@ public class Pet extends BaseEntity{
     public String name;
 
 
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Set<Visit> getVisits() {
-        return visits;
-    }
-
-    public void setVisits(Set<Visit> visits) {
-        this.visits = visits;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/Specialty.java
@@ -1,8 +1,16 @@
 package guru.framework.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 @Entity
 @Table(name = "specialties")
@@ -11,11 +19,4 @@ public class Specialty extends BaseEntity{
     @Column(name = "description")
     private String description;
 
-    public String getDiscription(){
-        return description;
-    }
-
-    public void setDescription(String description){
-        this.description = description;
-    }
 }
