622a00aa719f67cd9a9f09405c8e6fb349948694
seetide <seetide@gmail.com>
2019-11-29 14:01:32 -0500

    add model attribute for PetController.

--- pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/repositories/PetRepository.java
@@ -1,7 +1,11 @@
 package guru.framework.sfgpetclinic.repositories;
 
 import guru.framework.sfgpetclinic.model.Pet;
+import guru.framework.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface PetRepository extends CrudRepository<Pet, Long> {
+
 }
---
+++ pet-clinic-web/src/main/java/guru/framework/sfgpetclinic/controllers/PetController.java
@@ -0,0 +1,60 @@
+package guru.framework.sfgpetclinic.controllers;
+
+import guru.framework.sfgpetclinic.model.Owner;
+import guru.framework.sfgpetclinic.model.Pet;
+import guru.framework.sfgpetclinic.model.PetType;
+import guru.framework.sfgpetclinic.service.OwnerService;
+import guru.framework.sfgpetclinic.service.PetService;
+import guru.framework.sfgpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATED_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService pets, OwnerService owners, PetTypeService petTypeService) {
+        this.petService = pets;
+        this.ownerService = owners;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return this.petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return this.ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @InitBinder("pet")
+    public void initPetBinder(WebDataBinder dataBinder){
+        dataBinder.setValidator(new PetValidator());
+    }
+
+    @GetMapping("/pets/new")
+    public String initCreationForm(Owner owner, Model model){
+        Pet pet = new Pet();
+        owner.addPet(pet);
+        model.addAttribute("pet", pet);
+        return VIEWS_PETS_CREATED_OR_UPDATE_FORM;
+    }
+
+
+}
